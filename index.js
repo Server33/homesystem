d3f9d0b0cda5c7e6a26a3f4e404a1e21c1fe38ce
Server33 <ibrahim.oezkan2@gmail.com>
2021-01-26 19:38:32 +0100

    text

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ var PORT = process.env.PORT || 3000;
 const io = require("socket.io")(PORT);
 
 io.on("connection", function (socket) {
-    console.log("A user connected");
+    
     //socket.emit("addFinger",'{"type":"changeMod","currentMod":"addFinger","fingerId":"2"}');
     socket.on("chipMessage",(msg) =>{
-        console.log(msg.fingerId);
+        socket.emit("herokuApp","connection success");
     });
+    socket.on("appMessage",(msg)=>{
+        socket.emit("herokuApp","connection success");
+    })
 })
 
 io.on("disconnect",()=>{
